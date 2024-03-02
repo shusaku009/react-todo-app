69c42da5fa7347560f45c3ace43df2d0aba257cb
shusaku009 <shusaku009@gmail.com>
2024-03-02 20:57:22 +0900

    Add: 検証ルールを追加

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StateTodo from './StateTodo';
 import FormBasic from './FormBasic';
+import FormYup from './FormYup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     {/* <App /> */}
     <StateTodo />
     <FormBasic />
+    <FormYup />
   </React.StrictMode>
 );
 
