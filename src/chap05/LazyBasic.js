3f0ed4cb62f873eaee2bc5def88f8985fde18323
shusaku009 <shusaku009@gmail.com>
2024-03-03 21:06:10 +0900

    Add: Suspenseコンポーネント

---
+++ src/chap05/LazyBasic.js
@@ -0,0 +1,13 @@
+import { lazy, Suspense } from "react";
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const LazyButton = lazy(() => sleep(2000).then(() => import('./LazyButton')));
+
+export default function LazyBasic() {
+  return (
+    <Suspense fallback={<p>Now Loading...</p>}>
+      <LazyButton />
+    </Suspense>
+  )
+}
