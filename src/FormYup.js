72f8af1665b3f2760335ccfb7dbd569ff3c5beae
shusaku009 <shusaku009@gmail.com>
2024-03-03 11:58:24 +0900

    Update: 汎用的な検証ルールを追加する

--- src/FormYup.js
+++ src/FormYup.js
@@ -2,6 +2,20 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup'
 
+yup.addMethod(yup.string, 'ng', function() {
+  return this.test('ng',
+    ({ label }) => `${label}にNGワードが含まれています`,
+    value => {
+      const ngs = ['暴力', '死', 'グロ'];
+      for (const ng of ngs) {
+        if (value.includes(ng)) {
+          return false;
+        }
+      }
+      return true;
+    })
+})
+
 const schema = yup.object({
   name: yup
     .string()
@@ -22,17 +36,7 @@ const schema = yup.object({
     .label('備考')
     .required('${label}は必須入力です。')
     .min(10, '${label}は${min}文字以上で入力してください。')
-    .test('ng',
-      ({ label }) => `${label}にNGワードが含まれています`,
-      value => {
-        const ngs = ['暴力', '死', 'グロ'];
-        for (const ng of ngs) {
-          if (value.includes(ng)) {
-            return false;
-          }
-        }
-        return true;
-      })
+    .ng()
 });
 
 function FormYup() {
