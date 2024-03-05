6b273c057f1bf8e24a4dcd1856a4feb0484f89e0
shusaku009 <shusaku009@gmail.com>
2024-03-05 09:08:30 +0900

    Update: コンポーネントの読み込みを修正

--- src/chap05/SuspenseSimple.js
+++ src/chap05/SuspenseSimple.js
@@ -1,11 +1,10 @@
 import { Suspense } from "react";
 import ThrowPromise from './ThrowPromise';
-import ThrowResult from './ThrowResult';
 
 function SuspenseSimple() {
   return (
     <Suspense fallback={<p>Now Loading...</p>}>
-      <ThrowResult />
+      <ThrowPromise />
     </Suspense>
   )
 }
