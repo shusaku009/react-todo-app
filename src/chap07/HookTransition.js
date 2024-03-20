8996f772f0f6215e1b850df47de7b0b2837ea345
shusaku009 <shusaku009@gmail.com>
2024-03-20 11:17:10 +0900

    Add: useTransitionを追加

--- src/chap07/HookTransition.js
+++ src/chap07/HookTransition.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import books from './books';
 import commentList from './comments'
 import { BookDetails, CommentList } from './HookTransitionChild';
@@ -6,10 +6,13 @@ import { BookDetails, CommentList } from './HookTransitionChild';
 export default function HookTransition() {
   const [isbn, setIsbn] = useState('');
   const [comments, setComments] = useState([]);
+  const [isPending, startTransition] = useTransition();
   const handleChange = e => {
     const isbn = e.target.value;
     setIsbn(isbn);
-    setComments(commentList.filter(c => c.isbn === isbn));
+    startTransition(() => {
+      setComments(commentList.filter(c => c.isbn === isbn));
+    })
   }
 
   return (
