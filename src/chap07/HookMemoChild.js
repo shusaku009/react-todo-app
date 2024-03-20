cf6025932fad60a098ab44191b815b08a52e7543
shusaku009 <shusaku009@gmail.com>
2024-03-20 10:15:38 +0900

    Add: memo関数を使用

--- src/chap07/HookMemoChild.js
+++ src/chap07/HookMemoChild.js
@@ -1,13 +1,19 @@
-export const MyButton = ({ id, handleClick, children }) => {
-  console.log(`MyButton is called: ${id}`);
-  return (
-    <button onClick={handleClick}>{children}</button>
-  );
-}
+import { memo } from 'react';
 
-export const MyCounter = ({ id, value }) => {
-  console.log(`MyCounter is called: ${id}`);
-  return (
-    <p>現在値：{value}</p>
-  );
-};
+export const MyButton = memo(
+  ({ id, handleClick, children }) => {
+    console.log(`MyButton is called: ${id}`);
+    return (
+      <button onClick={handleClick}>{children}</button>
+    );
+  }
+);
+
+export const MyCounter = memo(
+  ({ id, value }) => {
+    console.log(`MyCounter is called: ${id}`);
+    return (
+      <p>現在値：{value}</p>
+    );
+  }
+);
